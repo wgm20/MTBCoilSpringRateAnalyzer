5c2391eb231fd60faac6199d842406d54b2e4ea8
wgm20 <42830420+wgm20@users.noreply.github.com>
2023-09-30 13:21:25 +0100

    Update CoilSpringRateComparisons_Streamlit.py

--- CoilSpringRateComparisons_Streamlit.py
+++ CoilSpringRateComparisons_Streamlit.py
@@ -18,7 +18,7 @@ def motion_ratio(travel, stroke):
 def spring_rate_at_wheel(spring, motion_ratio):
     return spring/(motion_ratio**2)
 
-def spring_rate_at_wheel_normalised_75kg(spring_rate_at_wheel, weight):
+def spring_rate_at_wheel_normlaised_75kg(spring_rate_at_wheel, weight):
     return spring_rate_at_wheel*75/weight
 
 def energy_at_max_travel(spring_rate, shock_stroke):
@@ -33,7 +33,7 @@ def add_label(name, spring):
 def add_calucated_quantitles(df):
     df['Motion_ratio'] = motion_ratio(df['Travel'], df['Stroke'])
     df['Spring_rate_at_wheel'] = spring_rate_at_wheel(df['Spring_rate'],df['Motion_ratio'])
-    df['Spring_rate_at_wheel_normalised_75kg'] = spring_rate_at_wheel_normalised_75kg(df['Spring_rate_at_wheel'],df['Weight'])
+    df['Spring_rate_at_wheel_normalised_75kg'] = spring_rate_at_wheel_normlaised_75kg(df['Spring_rate_at_wheel'],df['Weight'])
     df['Energy_at_max_travel'] = energy_at_max_travel(df['Spring_rate'], df['Stroke'])
     df['Huck_height_(m)'] = huck_height(df['Energy_at_max_travel'], df['Weight'])
     df['LabelX'] = np.vectorize(add_label)(df['Name'], df['Spring_rate'])
@@ -41,12 +41,11 @@ def add_calucated_quantitles(df):
 
 
 # Title
-st.markdown("<h1 style='font-size: 54px;'>Setup analyzer</h1>", unsafe_allow_html=True)
-st.markdown("<h1 style='font-size: 40px;'>Uncover your ideal configuration</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='font-size: 54px;'>Setup analyzer: Uncover your ideal configuration</h1>", unsafe_allow_html=True)
 # Description
 st.markdown("""
 <sub style='font-size: 16px;'>
-Enter your details on the left and change the spring rate until you find an appropriate spring for your mountain bike, travel and weight.
+To use: enter your details on the left and change the spring rate until you find an appropriate spring for your mountain bike, travel and weight.
 </sub>
 """, unsafe_allow_html=True)
 # Credit
@@ -65,7 +64,7 @@ st.write("\n")
 st.title("Coil Spring Rate Comparisons")
 st.write("\n")
 st.sidebar.markdown("### Setup Analyzer")
-st.sidebar.markdown("Enter your details...")
+st.sidebar.markdown("To use: enter your details on the left...")
 
 user_discipline = st.sidebar.selectbox("Discipline", ['Enduro', 'DH'])
 user_stroke = st.sidebar.slider("Rear shock stroke (mm)", 20.0, 100.0, 63.0, 0.5)  # All values are floats
@@ -74,8 +73,9 @@ user_weight = st.sidebar.slider("Rider weight (Kg)", 20.0, 200.0, 80.0, 1.0)  # 
 user_spring_rate = st.sidebar.slider("Spring_rate lbs/in", 200.0, 800.0, 434.0, 5.0)  # All values are floats
 user_speed_rating = st.sidebar.slider("Rider speed, Mens WCDH = 10", 1.0, 10.0, 5.0, 1.0)  # All values are floats
 user_name = st.sidebar.text_input("Name", "Jane Doe")
-
 # Data Preparation and Calculation
+# Your data reading and calculations here...
+
 df = pd.read_csv("Data.csv", index_col=1)
 df = df.reset_index()
 
@@ -98,17 +98,19 @@ df['plot_point_size'] = 2
 df_combined = pd.concat([df, df_user])
 
 
+
 # Make the chart
 points = alt.Chart(df, title='Normalised Rider Weight Spring Rate vs Bike Max Travel').mark_circle().encode(
     alt.X('Travel:Q',axis=alt.Axis(title ='Travel_')).scale(zero=False),
     alt.Y('Spring_rate_at_wheel_normalised_75kg:Q', axis=alt.Axis(title ='Spring_rate_at_wheel_normalised_75kg_')).scale(zero=False),
-    color='Discipline:N',
+    color=alt.Color('Discipline:N', scale=alt.Scale(domain=['Enduro', 'DH', 'Entered Data', 'best fit line'], range=['blue', 'green', 'red', 'orange'])),
+    size='Speed_rating:Q',
     tooltip=['Name', 'Weight', 'Spring_rate', 'Speed_rating', 'Discipline'],
 )
 points_user = alt.Chart(df_user, title='Normalised Rider Weight Spring Rate vs Bike Max Travel').mark_circle().encode(
     alt.X('Travel:Q',axis=alt.Axis(title ='Travel_')).scale(zero=False),
     alt.Y('Spring_rate_at_wheel_normalised_75kg:Q', axis=alt.Axis(title ='Spring_rate_at_wheel_normalised_75kg_')).scale(zero=False),
-    color='Discipline:N',
+    color=alt.Color('Discipline:N', scale=alt.Scale(domain=['Enduro', 'DH', 'Entered Data', 'best fit line'], range=['blue', 'green', 'red', 'orange'])),
     tooltip=['Name', 'Weight', 'Spring_rate', 'Speed_rating', 'Discipline'],
     size=alt.value(200)
 )
@@ -120,7 +122,8 @@ labels =  alt.Chart(df_combined).mark_text(align='left', baseline='middle', dx=4
 reg = alt.Chart(df).mark_circle().encode(
     alt.X('Travel:Q',axis=alt.Axis(title ='Travel_')).scale(zero=False),
     alt.Y('Spring_rate_at_wheel_normalised_75kg:Q', axis=alt.Axis(title ='Spring_rate_at_wheel_normalised_75kg_')).scale(zero=False),
-    color='Discipline:N',
+    color=alt.Color('Discipline:N', scale=alt.Scale(domain=['Enduro', 'DH', 'Entered Data', 'best fit line'], range=['blue', 'green', 'red', 'orange'])),
+    size='Speed_rating:Q',
     tooltip=['Name', 'Weight', 'Spring_rate', 'Speed_rating', 'Discipline']
 ).transform_regression('Travel', 'Spring_rate_at_wheel_normalised_75kg').mark_line(
      opacity=0.50, 
@@ -141,7 +144,8 @@ charts = (points + points_user + reg + labels).properties(width="container").int
 huck_height_chart = alt.Chart(df, title='Huck_height_(m) ').mark_circle().encode(
     alt.X('Travel:Q').scale(zero=False),
     alt.Y('Huck_height_(m):Q').scale(zero=False),
-    color='Discipline:N',
+    color=alt.Color('Discipline:N', scale=alt.Scale(domain=['Enduro', 'DH', 'Entered Data', 'best fit line'], range=['blue', 'green', 'red', 'orange'])),
+    size='Speed_rating:Q',
     tooltip=['Name', 'Weight', 'Spring_rate', 'Speed_rating', 'Discipline']
 ).properties(
     width="container"
@@ -150,9 +154,9 @@ huck_height_chart = alt.Chart(df, title='Huck_height_(m) ').mark_circle().encode
 huck_height_chart_user = alt.Chart(df_user, title='Huck_height_(m) ').mark_circle().encode(
     alt.X('Travel:Q').scale(zero=False),
     alt.Y('Huck_height_(m):Q').scale(zero=False),
-    color='Discipline:N',
+    color=alt.Color('Discipline:N', scale=alt.Scale(domain=['Enduro', 'DH', 'Entered Data', 'best fit line'], range=['blue', 'green', 'red', 'orange'])),
+    size='Speed_rating:Q',
     tooltip=['Name', 'Weight', 'Spring_rate', 'Speed_rating', 'Discipline'],
-    size=alt.value(200)
 ).properties(
     width="container"
 )
@@ -164,7 +168,7 @@ labels_h =  alt.Chart(df_combined).mark_text(align='left', baseline='middle', dx
 reg_h = alt.Chart(df).mark_circle().encode(
     alt.X('Travel:Q',axis=alt.Axis(title ='Travel_')).scale(zero=False),
     alt.Y('Huck_height_(m):Q', axis=alt.Axis(title ='Huck_height_(m)')).scale(zero=False),
-    color='Discipline:N',
+    color=alt.Color('Discipline:N', scale=alt.Scale(domain=['Enduro', 'DH', 'Entered Data', 'best fit line'], range=['blue', 'green', 'red', 'orange'])),
     tooltip=['Name', 'Weight', 'Spring_rate', 'Speed_rating', 'Discipline']
 ).transform_regression('Travel', 'Huck_height_(m)').mark_line(
      opacity=0.50, 
@@ -188,11 +192,6 @@ st.altair_chart(charts2)
 
 st.markdown("""
 ### Definitions:
-
-- **Spring_rate_at_wheel_normalised_75kg:**  
-    This metric adjusts the spring rate of different setups to make them comparable as if a 75kg rider were using them. 
-    By standardizing the setups to a 75kg rider, you can easily compare the stiffness across different rider weights. 
-    For instance, if a 90kg rider uses a 500lbs/in spring, this would feel similar to a 75kg rider using a 416lbs/in spring.
 
 - **Huck_height:**  
     Huck_height is the height you could drop rider and bike from and all energy be contained in the spring without bottoming out (assumes 60% of weight on rear wheel)
